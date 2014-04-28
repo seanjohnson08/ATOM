cb5a8f602c774be4f22fa26fbb9c68b130fe64e5
Sean Johnson <seanjohnson08@gmail.com>
2014-04-28 15:09:48 -0400

    Reinforced file uglification order

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -3,7 +3,11 @@ module.exports = {
         files: [
             {
                 src: [
+                    //Order is very important here!
                     '<%= workingPath.paths() %>/includes/JS/lib/jquery.js',
+                    '<%= workingPath.paths() %>/includes/JS/lib/jquery-migrate-1.2.1.js',
+                    '<%= workingPath.paths() %>/includes/JS/lib/jquery.ba-postmessage.min.js',
+                    '<%= workingPath.paths() %>/includes/JS/main.js',
                     '<%= workingPath.paths() %>/includes/JS/lib/*.js',
                     '<%= workingPath.paths() %>/includes/JS/lib/deux/*.js',
                 ],
@@ -11,7 +15,9 @@ module.exports = {
             }
         ],
         options: {
-            mangle: true,
+            mangle: {
+                except: ['jQuery']
+            },
             sourceMap: true,
             // sourceMapIncludeSources: true,
             sourceMapName: '<%= workingPath.paths() %>/includes/JS/common.map.js'
