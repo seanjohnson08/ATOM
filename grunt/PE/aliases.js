abecb48b7ee47e924ac9190302922d3a4401340f
Sean Johnson <seanjohnson08@gmail.com>
2014-05-13 18:37:39 -0400

    Fixed PE watch

--- grunt/PE/aliases.js
+++ grunt/PE/aliases.js
@@ -1,7 +1,5 @@
 module.exports = function(grunt, options) {
     grunt.registerTask('default', 'uglify:compress');
 
-    grunt.registerTask('listen', function() {
-        grunt.task.run('parallel');
-    });
+    grunt.registerTask('listen', 'watch');
 };
